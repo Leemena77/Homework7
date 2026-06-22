e5d7027c59fa31a23ef4126bbc0b2c4e6f114340
Leemena77 <elizavetaivanova475@gmail.com>
2026-06-22 12:19:10 +0300

    Выполненное ДЗ

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,86 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        System.out.println("Task 1");
+        int firstFriday = 1;
+        for (int i = 1; i <= 31; i++) {
+            if (i == firstFriday) {
+                System.out.println("Сегодня пятница, " + firstFriday + " число. Необходимо подготовить отчет");
+                firstFriday += 7;
+            }
+            System.out.println("Task 2");
+            int distance = 0;
+            do {
+                System.out.println("Держитесь! Осталось " + distance + " метров");
+                distance += 500;
+            } while (distance <= 42195);
+        }
+        System.out.println("Tas 2 - for");
+        int distance = 0;
+        for (int i = 0; i <= 42195; i = i + 500) {
+            System.out.println("Держитесь! Осталось " + i + " метров");
+        }
+        System.out.println("Task 3");
+        int sum = 1500;
+        int day = 1;
+        while (true) {
+            if (day % 5 == 0) {
+                day++;
+                System.out.println("Бесплатный день");
+                continue;
+            }
+            sum -= 100;
+            System.out.println("День " + day + " : Списание 100. Остаток " + sum);
+
+            day++;
+            if (sum <= 100) {
+                System.out.println("Пополните счет");
+                break;
+            }
+        }
+        System.out.println("Task 3 - for");
+        int sum2 = 1500;
+        int days = 1;
+        for (int i = sum2; i >= 100; days++) {
+            if (days % 5 == 0) {
+                System.out.println("День " + days + " : Бесплатный день. Остаток " + i);
+            } else {
+                i -= 100;
+                System.out.println("День " + days + " : Списание 100. Остаток " + i);
+            }
+            System.out.println("Task 4");
+            int month = 0;
+            double total = 0;
+            int monthMoney = 15000;
+            while (true) {
+                month++;
+                if (month % 6 == 0) {
+                    total = total + total / 100 * 7;
+                }
+                total = total + monthMoney;
+                if (total >= 12000000) {
+                    break;
+                }
+                System.out.println("Месяц " + month + " . Накоплений" + total);
+            }
+            System.out.println("Цель достигнута, понадобилось " + month + " месяцев");
+        }
+        System.out.println("Task 5");
+        int charge = 20;
+        int minute = 0;
+        int overheats = 0;
+        while (charge < 100 && overheats <= 3) {
+            minute++;
+            charge = charge + 2;
+            if (minute % 10 == 0) {
+                overheats++;
+                continue;
+            }
+            if (overheats == 3) {
+                System.out.println("Зарядка прекращена. Текущий заряд " + charge);
+                break;
+            }
+        }
+        System.out.println("Время зарядки составило " + minute);
     }
 }
